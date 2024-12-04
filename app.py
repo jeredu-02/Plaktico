c26ebfcda44c527af11c38260111c273d1b1d70c
jeredu-02 <jeremy.lujan@utec.edu.pe>
2024-12-04 04:55:19 -0500

    requerimientos_2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from main import MultiLabelClassifier
 from flask_cors import CORS
 import os
 import uuid
+import tempfile
 
 # Define las etiquetas
 labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
@@ -13,6 +14,7 @@ classifier.load_model('multi_label_classifier.pkl')
 
 # Inicializa la aplicación Flask
 app = Flask(__name__)
+temp_dir = tempfile.gettempdir()
 CORS(app)
 
 # Ruta para verificar si la API está activa (GET)
@@ -24,33 +26,24 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image part in the request"}), 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    # Guarda la imagen en el directorio temporal
+    file_path = os.path.join(temp_dir, file.filename)
+    file.save(file_path)
+
+    # Procesa la imagen
     try:
-        if 'image' not in request.files:
-            return jsonify({'error': 'No se proporcionó una imagen'}), 400
-
-        # Generar un nombre único para el archivo
-        image = request.files['image']
-        unique_filename = f"./temp/{uuid.uuid4().hex}.jpg"
-        image.save(unique_filename)
-        print(f"Imagen guardada en {unique_filename}")
-
-        # Realizar la predicción
-        predicted_label, confidence = classifier.predict(unique_filename)
-        print(f"Predicción: {predicted_label}, Confianza: {confidence}")
-
-        threshold = 0.90  # Ajusta según sea necesario
-        if confidence < threshold:
-            predicted_label = "None" 
-
-        # Eliminar la imagen temporal
-        os.remove(unique_filename)
-        print("Imagen eliminada.")
-
-        return jsonify({'predicted_label': predicted_label, 'confidence': confidence})
-
+        predicted_label, confidence = classifier.predict(file_path)
+        os.remove(file_path)  # Limpia el archivo temporal después de procesarlo
+        return jsonify({"predicted_label": predicted_label, "confidence": confidence})
     except Exception as e:
-        print(f"Error en el servidor: {str(e)}")
-        return jsonify({'error': 'Error interno del servidor', 'details': str(e)}), 500
+        return jsonify({"error": "Error interno del servidor", "details": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
